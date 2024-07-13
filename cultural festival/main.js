c3cca35864b9e69cfe357bb13fa54cccc778ad8c
gfhajkafjhdfk <166894544+gfhajkafjhdfk@users.noreply.github.com>
2024-07-13 15:08:52 +0900

    Update main.js

--- cultural festival/main.js
+++ cultural festival/main.js
@@ -1,6 +1,6 @@
-$(window).on('load',(function(){
-    setTimeout(function(){
-    $('.loading').stop().fadeOut();
-    $('.wrap').addClass('on');
-    },5000);
+$(window).on('load', function() {
+    setTimeout(function() {
+        $('.loading').stop().fadeOut();
+        $('.wrap').addClass('on');
+    }, 5000); // 5000ミリ秒（5秒）の遅延を追加
 });
