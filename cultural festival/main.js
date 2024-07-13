078598b5fea95d5dbe576f42a5d3b5b0b8613968
gfhajkafjhdfk <166894544+gfhajkafjhdfk@users.noreply.github.com>
2024-07-13 15:04:36 +0900

    Update main.js

--- cultural festival/main.js
+++ cultural festival/main.js
@@ -1,6 +1,6 @@
-$(window).on('load',(function(){
+$(window).on('load',function(){
     setTimeout(function(){
     $('.loading').stop().fadeOut();
     $('.wrap').addClass('on');
-    },50);
+    },5000);
 });
