5f15ea80569b660f28ebb1cd891b823e865b04c0
Hara1208 <133476221+Hara1208@users.noreply.github.com>
2024-07-21 14:36:16 +0900

    Update main.js

--- cultural festival/main.js
+++ cultural festival/main.js
@@ -2,5 +2,5 @@ $(window).on('load', function() {
     setTimeout(function() {
         $('.loading').stop().fadeOut();
         $('.wrap').addClass('on');
-    }, 5000); // 5000ミリ秒（5秒）の遅延を追加
+    }, 2500);
 });
